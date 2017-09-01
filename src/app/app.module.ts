92c59196290cffadad02ab8fa336614731b18128
gocepoposki <gocepoposki@yahoo.com>
2017-09-01 15:26:00 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {OcrComponent} from './ocr/ocr.component';
+import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import {ImageUploadModule} from 'angular2-image-upload';
+import {ImageCropperComponent, CropperSettings} from 'ng2-img-cropper';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OcrComponent,
+    ImageCropperComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
