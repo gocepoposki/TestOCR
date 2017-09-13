38b5e3b400f19d843910e606a75963e05eb4e098
gocepoposki <gocepoposki@yahoo.com>
2017-09-13 16:09:04 +0200

    add service

---
+++ src/app/ocr.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {Headers, Response, Http} from "@angular/http";
+import {Observable} from "rxjs";
+
+@Injectable()
+export class OcrService {
+
+  constructor(private http: Http) {
+  }
+
+  private baseUrl = "http://localhost:3000/plates";
+  private getHeaders: Headers = new Headers({'Content-Type': 'application/json'});
+
+  postImg(img) {
+    var body = JSON.stringify({
+      image: img,
+      country_code: 'eu',
+      pattern_code: 'no'
+    });
+    // return this.http.post(this.baseUrl + url, {headers: this.getHeaders})
+    return this.http.post(this.baseUrl, body, {headers: this.getHeaders})
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'))
+  }
+
+
+
+}
